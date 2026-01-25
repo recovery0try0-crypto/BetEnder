6781324a8318f8f87f53ac0c7b9305915db4aaf2
sequelinquiryDev <sequelinquiry@gmail.com>
2026-01-25 19:05:45 +0200

    Update dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,22 +1,27 @@
-{pkgs}: {
-        channel = "stable-24.05";
-        packages = [
-          pkgs.nodejs_latest
-        ];
-        idx.extensions = [
-          "svelte.svelte-vscode"
-          "vue.volar"
-        ];
-       idx.previews = {
-         previews = {
-           web = {
-             command = [
-               "npm"
-               "run"
-               "dev"
-             ];
-             manager = "web";
-           };
-         };
-       };
-     }
+{ pkgs }: {
+  channel = "stable-24.05";
+
+  packages = [
+    pkgs.nodejs_latest
+  ];
+
+  idx.extensions = [
+    "dbaeumer.vscode-eslint"
+    "esbenp.prettier-vscode"
+    "ms-vscode.vscode-typescript-next"
+    "bradlc.vscode-tailwindcss"
+  ];
+
+  idx.previews = {
+    previews = {
+      client = {
+        command = [ "npm" "run" "dev:client" ];
+        manager = "web";
+      };
+      server = {
+        command = [ "npm" "run" "dev:server" ];
+        manager = "process";
+      };
+    };
+  };
+}
